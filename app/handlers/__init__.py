c54ca6557ba8cebd19a259fa60d9ad2c93b01aed
Aleksei Panfilov <lexa97@gmail.com>
2026-01-30 12:34:20 +0300

    Refactor router initialization in handlers to simplify code structure

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -2,10 +2,10 @@ from aiogram import Router
 
 from app.handlers import start, group, film_search, list_watched
 
-router = Router(name="root")
-router.include_router(start.router, name="start")
-router.include_router(group.router, name="group")
-router.include_router(film_search.router, name="film_search")
-router.include_router(list_watched.router, name="list_watched")
+router = Router()
+router.include_router(start.router)
+router.include_router(group.router)
+router.include_router(film_search.router)
+router.include_router(list_watched.router)
 
 __all__ = ["router"]
